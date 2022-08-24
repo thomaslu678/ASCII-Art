f9d82afd0ae7c0cd479724f5a6b245e8c9fd9205
thomaslu678 <thomaslu678@gmail.com>
2022-08-23 21:24:35 -0600

    Initial commit

---
+++ src/AsciiArt.java
@@ -0,0 +1,11 @@
+public class AsciiArt {
+    public static void main(String[] args) {
+        System.out.println("    (>'v')>|      ");
+        System.out.println("_______]   |      ");
+        System.out.println("_______]   |      ");
+        System.out.println("_______]   |      ");
+        System.out.println("_______]   o      ");
+        System.out.println("_______]  ^^('^' )");
+
+    }
+}
